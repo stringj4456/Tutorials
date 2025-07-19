197f2e842affaf3468450d78f7dc1d10f7e68a56
StingJ <justinsaechao23@gmail.com>
2025-07-19 15:30:49 -0700

    Simple exceptions example

---
+++ Exceptions/exceptions.py
@@ -0,0 +1,15 @@
+import math
+
+num_list = [10, -5, 1.2, "apple"]
+
+for num in num_list:
+    try:
+        num_factorial = math.factorial(num)
+    except TypeError:
+        print("Factorial is not supported for the given input type:", num)
+    except ValueError:
+        print("Factorial only accepts positive integer values.", num, "is not a positive integer")
+    else:
+        print("The factorial of", num, "is", num_factorial)
+    finally:
+        print("We are done now.")
