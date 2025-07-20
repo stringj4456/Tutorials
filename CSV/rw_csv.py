ecaeb2c2ab1f9f55e1491cfe9bbd00e7b3add291
StingJ <justinsaechao23@gmail.com>
2025-07-19 17:22:57 -0700

    Added csv.writer section

--- CSV/rw_csv.py
+++ CSV/rw_csv.py
@@ -8,10 +8,17 @@ with open("username.csv", newline="") as File:
         print(row)
 
 print()
-print("DictReader Class Example:")
+
 # Example using the DictReader class
+print("DictReader Class Example:")
 with open("username.csv", newline="") as csvfile:
     reader = csv.DictReader(csvfile, delimiter=";")
     for row in reader:
-        print(row["Username"],row["Identifier"])
+        print(row)
+
+# Example writing using the csv.writer function
+state_info = ["California", "Sacramento", "Los Angeles", "39538223"]
+with open("state_data.csv", "a", newline="") as state_file:
+    writer = csv.writer(state_file)
+    writer.writerow(state_info)
 
