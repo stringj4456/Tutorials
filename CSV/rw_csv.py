76bd4a8d9bd4aaa08e058dc8ff89aa503e36e922
StingJ <justinsaechao23@gmail.com>
2025-07-20 15:26:50 -0700

    Added DictWriter

--- CSV/rw_csv.py
+++ CSV/rw_csv.py
@@ -22,3 +22,28 @@ with open("state_data.csv", "a", newline="") as state_file:
     writer = csv.writer(state_file)
     writer.writerow(state_info)
 
+# Example using the DictWriter class
+fields = ['Name', 'Capital City', 'Largest City', 'Population']
+
+state_info = [
+    {
+        "Name": "Colorado",
+        "Largest City": "Denver",
+        "Capital City": "Denver",
+        "Population": "5773714"
+    }, {
+        "Name": "Connecticut",
+        "Largest City": "Hartford",
+        "Capital City": "Bridgeport",
+        "Population": "3605944"
+    }, {
+        "Name": "Delaware",
+        "Largest City": "Dover",
+        "Capital City": "Wilmington",
+        "Population": "989948"
+    }
+]
+
+with open("statedata.csv", "a", newline="") as filecsv:
+    writer = csv.DictWriter(filecsv, fields)
+    writer.writerows(state_info)
